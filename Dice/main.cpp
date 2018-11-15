359ddc9d72104022023fa6200b4e9ab652e390c8
Zhennian Liang <liangzn@gmail.com>
2018-11-14 23:28:23 -0800

    Initial Commit

---
+++ Dice/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Dice
+//
+//  Created by Zhennian Liang on 11/14/18.
+//  Copyright © 2018 Zhennian Liang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
